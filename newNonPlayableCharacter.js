4395324b9a056238928879b7d9c2681a9d532129
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2024-03-12 13:53:30 -0500

    adding Npc route

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -22,25 +22,33 @@ function newNonPlayableCharacter(x, y) {
     }
 
     setInterval(moveCharacter, 1)
-
-    function walkEast() {
-        direction = 'east'
-        element.src = `./assets/red-character/east.gif`
+    
+    async function walkEast(time) {
+        direction = 'east';
+        element.src = './assets/red-character/east.gif';
+        await sleep(time);
+        stop();
     }
 
-    function walkNorth() {
+    async function walkNorth(time) {
         direction = 'north'
-        element.src = `./assets/red-character/north.gif`
+        element.src = `./assets/red-character/north.gif`;
+        await sleep(time);
+        stop();
     }
 
-    function walkWest() {
+    async function walkWest(time) {
         direction = 'west'
-        element.src = `./assets/red-character/west.gif`
+        element.src = `./assets/red-character/west.gif`;
+        await sleep(time);
+        stop();
     }
 
-    function walkSouth() {
+    async function walkSouth(time) {
         direction = 'south'
-        element.src = `./assets/red-character/south.gif`
+        element.src = `./assets/red-character/south.gif`;
+        await sleep(time);
+        stop();
     }
 
     function stop() {
@@ -55,5 +63,12 @@ function newNonPlayableCharacter(x, y) {
         walkEast: walkEast,
         walkSouth: walkSouth,
         stop: stop
-    }
+    };
 }
+
+function sleep(time){
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    });
+}
+
